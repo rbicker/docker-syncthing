1ea4c6f4c082fb335d993a1501f59f6bd2b78389
Raphael Bicker <raphael@bicker.ch>
2020-04-10 19:44:23 +0200

    fix missing strelaysrv

--- entrypoint.sh
+++ entrypoint.sh
@@ -14,6 +14,8 @@ if [ ! -d $SRC_PATH ]; then
   mkdir -p $SRC_PATH && cd $SRC_PATH
   git clone https://github.com/syncthing/syncthing.git .
   go run build.go
+  go run build.go build strelaysrv
+  cp $SRC_PATH/strelaysrv /usr/local/bin/
   cp $SRC_PATH/bin/* /usr/local/bin/
 fi
 
